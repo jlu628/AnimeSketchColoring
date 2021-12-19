71f3a2eb3fcd5e97fbfff1b5ce6b68e5ee6b3170
Jerry <6jerrylu@gmail.com>
2021-12-19 08:45:44 -0500

    Add models file and create cnn block

---
+++ models.py
@@ -0,0 +1,15 @@
+import torch
+import torch.nn as nn
+
+
+class CNNBlock(nn.Module):
+    def __init__(self, in_channels, out_channels, stride):
+        super(CNNBlock, self).__init__()
+        self.conv = nn.Sequential(
+            nn.Conv2d(in_channels,out_channels, 4, stride, 1, padding_mode="reflect", bias=False),
+            nn.BatchNorm2d(out_channels),
+            nn.LeakyReLU(0.2)
+        )
+
+    def forward(self, x):
+        return self.conv(x)
